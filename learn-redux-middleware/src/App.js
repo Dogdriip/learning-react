625b6846a9375dfd13696d4f33f1f82f5bb9ec39
Hyunseung Jeon <dogdriip@gmail.com>
2021-04-06 12:25:45 +0900

    210406 18

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CounterContainer from "./containers/CounterContainer";
+
+const App = () => {
+  return (
+    <div className="App">
+      <CounterContainer />
+    </div>
+  );
+};
+
+export default App;
